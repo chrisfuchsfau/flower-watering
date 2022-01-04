d2e8bf9195ffc4702d6cdc418ca5da328feb2688
chrisfuchsfau <69166447+chrisfuchsfau@users.noreply.github.com>
2022-01-04 15:12:56 +0100

    update

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/01_programming-projects/Blumen-giessen
+# Install script for directory: C:/01_programming-projects/flower-watering
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/01_programming-projects/Blumen-giessen/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/01_programming-projects/flower-watering/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
